697bc416e62013a32883755c9919a0aeb2c03202
he1ex-tG <gosu.he1ex@gmail.com>
2022-07-28 22:28:00 +0500

    Initial commit

---
+++ src/main/kotlin/com/he1extg/pdfreader/PdfreaderApplication.kt
@@ -0,0 +1,11 @@
+package com.he1extg.pdfreader
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class PdfreaderApplication
+
+fun main(args: Array<String>) {
+	runApplication<PdfreaderApplication>(*args)
+}
---
+++ src/test/kotlin/com/he1extg/pdfreader/PdfreaderApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.he1extg.pdfreader
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class PdfreaderApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
